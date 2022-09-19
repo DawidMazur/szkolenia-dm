8591b0b2cd40981215319f6778f2ce41e7ede6a4
DawidMazur <50441054+DawidMazur@users.noreply.github.com>
2022-09-19 21:52:43 +0200

    php podstawy --done

--- template-php-code.php
+++ template-php-code.php
@@ -5,11 +5,11 @@
  */
 
 // tworzenie zmiennej
-
 $variable = "Wartość";
 $variable = 123;
 $variable = 123.10;
 
+// tablicy
 $variable = [
     'array'
 ];
@@ -17,3 +17,62 @@ $variable = [
 $variable = array();
 $variable[] = 'array';
 
+// funkcja w php
+function example_function($parametr = "default" ) {
+    return 'example_function <br>';
+}
+
+
+// tworzenie class w php
+class ExampleClass {
+    // zmienna obiektu klasy
+    public $public_var;
+    private $private_var;
+    public static $static_var;
+
+    // konstruktor
+    public function __construct() {
+        echo 'Wywołuję się podczas tworzenia obiektu <br>';
+
+        $this->public_var = "Public";
+        $this->private_var = "Private";
+        ExampleClass::$static_var = "Static";
+    }
+
+    // metody klasy
+    public function example_method() {
+        return 'example_method <br>';
+    }
+
+    public static function static_method() {
+        return 'static_method <br>';
+    }
+
+}
+
+// Jak w każdym języku do statycznych rzeczy można się odwołać bez obiektu
+echo ExampleClass::$static_var;
+echo ExampleClass::static_method();
+
+// Tworzenie obiektu klasy
+$example_Class = new ExampleClass();
+// wywołanie methody obiektu
+echo $example_Class->example_method();
+
+
+
+
+
+// Debugowanie zmiennych / obiektów
+// najprostrze możliwe
+print_r($variable);
+
+// to samo ale czytelniej
+echo '<pre>';
+print_r($variable);
+echo '</pre>';
+
+// z pomocą biblioteki kint-php
+d($variable);
+// biblioteka pozwala także debugować klasy
+d($example_Class);
