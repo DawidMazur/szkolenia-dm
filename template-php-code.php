0c4702821442f78b03b6cff9f13c9ca58d9589c6
DawidMazur <50441054+DawidMazur@users.noreply.github.com>
2022-09-19 20:58:40 +0200

    php code --start

---
+++ template-php-code.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Template Name: Przykładowy php kod
+ * @package ws
+ */
+
+// tworzenie zmiennej
+
+$variable = "Wartość";
+$variable = 123;
+$variable = 123.10;
+
+$variable = [
+    'array'
+];
+// w starszych wersjach
+$variable = array();
+$variable[] = 'array';
+
