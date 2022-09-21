107a83442faa487215bbda3c98155694ee048038
JakubGor <kubagorr@gmail.com>
2022-09-21 13:48:19 +0200

    Ajax przykład -Błąd w ajax.php - Do poprawy

--- functions/ajax.php
+++ functions/ajax.php
@@ -31,3 +31,52 @@
 //     ]);
 // }
 
+class AjaxEndpoints {
+    public static function init() {
+        AjaxEndpoints::add_ajax_endpoint('find_post');
+    }
+
+    private static function add_ajax_endpoint($name) {
+        add_action( 
+            'wp_ajax_nopriv_' . $name , [
+                'AjaxEndpoints',
+                $name,
+            ]
+        );
+        add_action( 
+            'wp_ajax_' . $name , [
+                'AjaxEndpoints',
+                $name,
+            ]
+        );
+    }
+
+    public static function find_post() {
+        $name = $_POST['data']['name'];
+
+        $posts = get_posts([
+            'post_type' => 'example_posts',
+            'post_per_page' => -1,
+            //'s' => $name,
+            'meta_query' => [
+                [
+                    'key' => 'info_test',
+                    'value' => $name,
+                    'compare' => 'LIKE',
+                ]
+            ]
+        ]);
+
+        $titles = [];
+
+        foreach($posts ?: [] as $post) {
+            $titles[] = $post->post_title;
+        }
+
+        wp_send_json([
+            'status' => 200,
+            'message' => $titles,
+        ]);
+    }
+}
+AjaxEndpoints::init();
--- views/components/flexible/parts/code/codes/alpineExample.js
+++ views/components/flexible/parts/code/codes/alpineExample.js
@@ -10,7 +10,26 @@ document.addEventListener('alpine:init', () => {
             this.loading = true;
             this.html = 'Ładuję: ' + this.name;
 
-            // todo ajax request
+            //zmianna pomocnicza
+            let form = this;
+
+            //wp_data.ajax -> przechowywane w themeFiles.php i zaciągane
+            jQuery.ajax({
+                type: "POST",
+                url: wp_data.ajax,
+                data: {
+                    action: 'find_post',
+                    data: {
+                        name: form.name,
+                    }
+                },
+
+                success: function(response) {
+                    console.log(response);
+                    form.html = response.message;
+                    form.loading = false;
+                }
+            });
         }
     }))
 })
